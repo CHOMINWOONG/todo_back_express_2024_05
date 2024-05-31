dfa592760d65b0bfc8a91caeb328911660b04bef
CHOMINWOONG <alsdnd0701@naver.com>
2024-05-31 20:13:36 +0900

    삭제

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const app = express();
 const port = 3000;
 
 const pool = mysql.createPool({
-    host : "localhost",
-    user : "korad",
-    password  : "kor123414",
-    database : "todo_2024_05",
-    waitForConnect: true,
+    host: "localhost",
+    user: "korad",
+    password: "kor123414",
+    database: "todo_2024_05",
+    waitForConnections: true,
     queueLimit: 0,
-    dataStrings: true
+    dateStrings: true,
 });
 
 var corsOptions = {
@@ -23,6 +23,7 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 
+// 리스트 조회
 app.get("/:user_code/todos", async (req, res) => {
     const { user_code } = req.params;
 
@@ -31,22 +32,24 @@ app.get("/:user_code/todos", async (req, res) => {
         SELECT *
         FROM todo
         WHERE user_code = ?
-        ORDER BY ID DESC
+        ORDER BY id DESC
         `,
         [user_code]
-    )
+    );
 
     res.json({
         resultCode: "S-1",
         msg: "성공",
         data: rows
     });
-})
+});
 
+
+// 단건 조회
 app.get("/:user_code/todos/:no", async (req, res) => {
     const { user_code, no } = req.params;
 
-    const [[ todoRow ] ] = await pool.query(
+    const [[ todoRow ]] = await pool.query(
         `
         SELECT *
         FROM todo
@@ -56,12 +59,12 @@ app.get("/:user_code/todos/:no", async (req, res) => {
         [user_code, no]
     );
 
-    if ( todoRow == undefined ){
+    if ( todoRow == undefined ) {
         res.status(404).json({
             resultCode: "F-1",
             msg: "not found",
         });
-        return
+        return;
     }
 
     res.json({
@@ -69,12 +72,52 @@ app.get("/:user_code/todos/:no", async (req, res) => {
         msg: "성공",
         data: todoRow
     });
-})
+});
 
-app.get("/todos",(req, res) => {
-    res.json([{ id: 1}, { id: 2 }]);
+
+// 삭제
+app.delete("/:user_code/todos/delete/:no", async (req, res) => {
+    const { user_code, no } = req.params;
+
+    const [[ todoRow ]] = await pool.query(
+        `
+        SELECT *
+        FROM todo
+        WHERE user_code = ?
+        AND no = ?
+        `,
+        [user_code, no]
+    );
+
+    if ( todoRow == undefined ) {
+        res.status(404).json({
+            resultCode: "F-1",
+            msg: "not found",
+        });
+        return;
+    }
+
+    await pool.query(
+        `
+        DELETE FROM todo
+        WHERE user_code = ?
+        AND no = ?
+        `
+        ,[user_code, no]
+    );
+
+    res.json({
+        resultCode: "S-1",
+        msg: `${no}번 할 일을 삭제하였습니다.`
+    });
+});
+
+
+
+app.get("/todos", (req, res) => {
+    res.json([{ id: 1 }, { id: 2 }]);
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 });
